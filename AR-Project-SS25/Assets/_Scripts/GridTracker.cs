53208520ca8fdad1353d5dfea11783501e48dcc5
Lara <73997200+yvmee@users.noreply.github.com>
2025-07-01 23:13:10 +0200

    multiple markers setup

--- AR-Project-SS25/Assets/_Scripts/GridTracker.cs
+++ AR-Project-SS25/Assets/_Scripts/GridTracker.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using TMPro;
 
 public class GridTracker : MonoBehaviour
 {
@@ -13,9 +15,14 @@ public class GridTracker : MonoBehaviour
 
     Dictionary<Vector2Int, string> occupiedCells = new();
     
+    private Dictionary<string, bool> markerVisibility = new();
+    
     // debugging
     [SerializeField] private GameObject cubePrefab;
     private Dictionary<TrackableId, GameObject> spawnedCubes = new();
+    
+    [SerializeField] private TextMeshProUGUI debugText;
+    private int counter = 0;
 
     void Awake()
     {
@@ -32,27 +39,70 @@ public class GridTracker : MonoBehaviour
         trackedImageManager.trackablesChanged.RemoveListener(OnChanged);
     }
 
+    private void Start()
+    {
+        debugText.text = "Started App";
+        
+        string[] markerNames = {
+            "Cell_0_0", "Cell_0_1", "Cell_0_2",
+            "Cell_1_0", "Cell_1_1", "Cell_1_2",
+            "Cell_2_0", "Cell_2_1", "Cell_2_2"
+        };
+
+        foreach (var _name in markerNames)
+        {
+            markerVisibility[_name] = false;
+        }
+    }
+
+    private void Update()
+    {
+        LogVisibleMarkers();
+    }
+
     void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
     {
         foreach (var newImage in eventArgs.added)
         {
-            GameObject cube = Instantiate(cubePrefab, newImage.transform);
-            cube.transform.localPosition = Vector3.zero; // center of the image
-            spawnedCubes[newImage.trackableId] = cube;
+            //GameObject cube = Instantiate(cubePrefab, newImage.transform);
+            //cube.transform.localPosition = Vector3.zero; // center of the image
+            //spawnedCubes[newImage.trackableId] = cube;
+            counter++;
+            
+            debugText.text = "Found new image. Count: " + counter + ", Id: " + newImage.trackableId.ToString();
         }
 
-        foreach (var updatedImage in eventArgs.updated)
+        foreach (var trackedImage in eventArgs.updated)
         {
-            // Handle updated event
+            string _name = trackedImage.referenceImage.name;
+
+            if (markerVisibility.ContainsKey(_name))
+            {
+                markerVisibility[_name] = trackedImage.trackingState == TrackingState.Tracking;
+            }
         }
 
         foreach (var removedImage in eventArgs.removed)
         {
-            if (spawnedCubes.TryGetValue(removedImage.Key, out var cube))
+            if (spawnedCubes.TryGetValue(removedImage.Key, out var cube))   
             {
                 Destroy(cube);
                 spawnedCubes.Remove(removedImage.Key);
+                --counter;
+                
+                debugText.text = "Lost image: " + removedImage.Key.ToString();
             }
         }
+    }
+    
+    void LogVisibleMarkers()
+    {
+        string text = "Visible Markers:\n";
+        foreach (var kvp in markerVisibility)
+        {
+            if (kvp.Value)
+                text += kvp.Key + "\n";
+        }
+        debugText.text = text;
     }
 }
